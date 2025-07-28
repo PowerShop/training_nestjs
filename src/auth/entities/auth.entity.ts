2965a5ff849ebbe8f96c106027dce55dfb8a8452
Kittisak Sakmeang <aaaxcvg@gmail.com>
2025-07-28 11:04:47 +0700

    modified table auth

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,7 +1,13 @@
+import { Column, Entity , PrimaryGeneratedColumn } from 'typeorm';
 
-
+@Entity('auth')
 export class Auth {
+    @PrimaryGeneratedColumn('increment')
     id: number;
+
+    @Column({ name: 'username', type: 'varchar', length: 50 })
     username: string;
+
+    @Column({ name: 'password', type: 'varchar', length: 255 })
     password: string;
 }
