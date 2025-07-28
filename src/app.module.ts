e66c8047c2af80ead72e9afde3882331f4ae000d
Kittisak Sakmeang <aaaxcvg@gmail.com>
2025-07-28 10:58:11 +0700

    new file:   .gitignore 	new file:   .prettierrc 	new file:   README.md 	new file:   eslint.config.mjs 	new file:   nest-cli.json 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app.controller.spec.ts 	new file:   src/app.controller.ts 	new file:   src/app.module.ts 	new file:   src/app.service.ts 	new file:   src/auth/auth.controller.spec.ts 	new file:   src/auth/auth.controller.ts 	new file:   src/auth/auth.module.ts 	new file:   src/auth/auth.service.spec.ts 	new file:   src/auth/auth.service.ts 	new file:   src/auth/dto/create-auth.dto.ts 	new file:   src/auth/dto/update-auth.dto.ts 	new file:   src/auth/entities/auth.entity.ts 	new file:   src/main.ts 	new file:   test/app.e2e-spec.ts 	new file:   test/jest-e2e.json 	new file:   tsconfig.build.json 	new file:   tsconfig.json

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'Kittisak644245001',
+    database: 'training_01',
+    entities: [],
+    synchronize: true,
+  }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
